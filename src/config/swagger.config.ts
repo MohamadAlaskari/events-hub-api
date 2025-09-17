941cf2eb3ed20227e3c86cb73e3819b45b1248d7
MohamadAlaskari <m_alaskari96@hotmail.com>
2025-09-17 23:46:35 +0300

    chor: change the setup place for static files for swagger-ui-dist

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,6 +1,7 @@
 import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-
+import { join } from 'path';
+import * as express from 'express';
 
 
 export function setupSwagger(app: INestApplication) {
@@ -15,4 +16,10 @@ export function setupSwagger(app: INestApplication) {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory, {
   jsonDocumentUrl: 'swagger/json',});
+
+   // static files for swagger-ui-dist
+  app.use(
+    '/swagger',
+    express.static(join(__dirname, '../node_modules/swagger-ui-dist')),
+  )
 }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
 
-import { join } from 'path';
 
-import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,11 +12,7 @@ async function bootstrap() {
 
   setupSwagger(app);
 
-    // Statische Swagger-UI Files bereitstellen
-  app.use(
-    '/swagger',
-    express.static(join(__dirname, '../node_modules/swagger-ui-dist')),
-  )
+   
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

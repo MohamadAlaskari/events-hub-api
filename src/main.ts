a5fb98677808aa6891270524bbe84e0e6d7fe284
MohamadAlaskari <m_alaskari96@hotmail.com>
2025-09-17 23:21:42 +0300

    fix deployment for swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
 
+import { join } from 'path';
+
+import * as express from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
@@ -9,6 +13,12 @@ async function bootstrap() {
    
 
   setupSwagger(app);
+
+    // Statische Swagger-UI Files bereitstellen
+  app.use(
+    '/swagger',
+    express.static(join(__dirname, '../node_modules/swagger-ui-dist')),
+  )
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
